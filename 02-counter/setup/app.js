459d83ea5dc4da86f4f4cb8cca4839617fb15b70
ATAMATODE <atamatode@gmail.com>
2022-05-21 14:11:15 -0500

    I update the js file

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -1,9 +1,4 @@
 let count = 0;
-<<<<<<< HEAD
-/*const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
-=======*/
-
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
@@ -30,27 +25,3 @@ btns.forEach(function (btn) {
         }
     })
 });
-/*
->>>>>>> 1068229993310ebf9ed6a0a02d06a59d731cf14d
-
-btns.forEach(function(btn){
-    btns.addEventListener("click",function(e){
-        const style = e.currentTarget.classList;
-        if (style.contains("decrease")){
-            count--;
-        }else if(style.contains("increase")){
-            count++;
-        }else{
-            count = 0;
-        }
-        //changing counter color 
-
-        if(count< 0){
-            value.style.color= "red";
-        }if(count>0){
-            value.style.color ="green";
-        }if(count == 0){
-            value.style.color ="black";
-        }
-    })
-})*/
